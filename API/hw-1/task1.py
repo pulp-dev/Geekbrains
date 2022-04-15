574fbaa492de77e17e8c7f0e9304a4c4d61e404a
pulp <petr8049@mail.ru>
2022-04-15 23:02:15 +0300

    hw

---
+++ API/hw-1/task1.py
@@ -0,0 +1,36 @@
+import json
+import requests
+
+SAVE_PATH = 'repos.json'
+TOKEN = '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'
+
+
+class GitHubUser:
+    def __init__(self, token, user_name):
+        self.token = token
+        self.user_name = user_name
+
+    # получить репозитории пользователя (вся информация о них)
+    def get_repos(self):
+        r = requests.get('https://api.github.com/user/repos', auth=(self.user_name, self.token))
+        return r.json()
+
+    # получить список названий репозиториев
+    @staticmethod
+    def get_names(r):
+        names = []
+        for i in r:
+            names.append(i['name'])
+        return names
+
+
+def pipeline(save_path, user_name):
+    user = GitHubUser(TOKEN, user_name)
+    r = user.get_repos()
+    names = user.get_names(r)
+    with open(save_path, 'w') as f:
+        json.dump(names, f, indent=2)
+
+
+if __name__ == '__main__':
+    pipeline(SAVE_PATH, input())
