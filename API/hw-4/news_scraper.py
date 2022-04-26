a3decae780ddd827e2491678f18156db7670cbbd
pulp <petr8049@mail.ru>
2022-04-26 15:25:40 +0300

    Yandex scraper is done; New class Scrape (Parent for classes LentaScraper, YandexNewsScraper)

--- API/hw-4/news_scraper.py
+++ API/hw-4/news_scraper.py
@@ -12,9 +12,32 @@ mongo_params = {
     'database': 'news_scraper',
     'collection': 'news'
 }
-URL = 'https://lenta.ru/'
+lenta_url = 'https://lenta.ru/'
+yandex_url = 'https://yandex.ru/news/'
 
 
+class Scraper:
+    def __init__(self, headers, url):
+        self.headers = headers
+        self.url = url
+
+    def make_request(self, url):
+        r = requests.get(url, self.headers)
+        return r
+
+    def get_date_from_page(self, url, date_lxml):
+        r = self.make_request(url)
+        dom = fromstring(r.text)
+        date = dom.xpath(date_lxml)
+        return date
+
+    @staticmethod
+    def insert_doc(doc):
+        mongo = MongoDBConnector(mongo_params['uri'], mongo_params['database'], mongo_params['collection'])
+        mongo.insert_doc(doc)
+
+
+# для работы с бд
 class MongoDBConnector:
     def __init__(self, uri, db, collection):
         self.uri = uri
@@ -34,7 +57,8 @@ class MongoDBConnector:
             collection.delete_many({})
 
 
-class LentaScraper:
+# для сайта lenta.ru
+class LentaScraper(Scraper):
     def __init__(self, headers, url):
         self.headers = headers
         self.url = url
@@ -42,23 +66,13 @@ class LentaScraper:
         self.title_lxml = ".//*[contains(@class, 'card-mini__title')]/text()"
         self.date_lxml = ".//*[contains(@class, 'card-mini__date')]"
 
-    def make_request(self, url):
-        r = requests.get(url, self.headers)
-        return r
-
-    def get_date_from_page(self, url):
-        r = self.make_request(url)
-        dom = fromstring(r.text)
-        date = dom.xpath("//time[@class='topic-header__item topic-header__time']")
-        return date
-
     def extract_info_into_doc(self, element):
         title = element.xpath(self.title_lxml)
         href = 'https://lenta.ru' + element.values()[1]
         try:
             date = element.xpath(self.date_lxml)[0].text
         except IndexError as e:
-            date = self.get_date_from_page(href)[0].text
+            date = self.get_date_from_page(href, "//time[@class='topic-header__item topic-header__time']")[0].text
 
         doc = {
             'Title': title[0],
@@ -74,14 +88,49 @@ class LentaScraper:
         dom = fromstring(r.text)
         news_containers = dom.xpath(self.news_lxml)
 
-        mongo = MongoDBConnector(mongo_params['uri'], mongo_params['database'], mongo_params['collection'])
-
         for el in news_containers:
             doc = self.extract_info_into_doc(el)
-            mongo.insert_doc(doc)
+            self.insert_doc(doc)
 
 
-scraper = LentaScraper(HEADERS, URL)
+class YandexNewsScraper(Scraper):
+    def __init__(self, headers, url):
+        self.headers = headers
+        self.url = url
+        self.news_lxml = "//*[contains(@class, 'mg-card_flexible')]"
+        self.title_lxml = './/h2[@class="mg-card__title"]//text()'
+        self.href_lxml = './/a[@class="mg-card__link"]'
+        self.date_lxml = ".//span[@class='mg-card-source__time']"
+        self.source_lxml = ".//span[@class='mg-card-source__source']//text()"
+
+    def extract_info_into_doc(self, element):
+        title = element.xpath(self.title_lxml)[0]
+        href = element.xpath(self.href_lxml)[0].values()[0]
+        date = element.xpath(self.date_lxml)[0].text
+        source = element.xpath(self.source_lxml)[0]
+
+        doc = {
+            'Title': title,
+            'URL': href,
+            'Date': date,
+            'Source': source
+        }
+
+        return doc
+
+    def pipeline(self):
+        r = self.make_request(self.url)
+        dom = fromstring(r.text)
+        news = dom.xpath(self.news_lxml)
+        for el in news:
+            doc = self.extract_info_into_doc(el)
+            self.insert_doc(doc)
+
+
+scraper = LentaScraper(HEADERS, lenta_url)
+scraper.pipeline()
+
+scraper = YandexNewsScraper(HEADERS, yandex_url)
 scraper.pipeline()
 
 # mongo = MongoDBConnector(mongo_params['uri'], mongo_params['database'], mongo_params['collection'])
